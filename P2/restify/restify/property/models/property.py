7fd71c620382ee55b1318e646ad01320eea3272a
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2023-03-11 11:48:43 -0500

    Update property.py

--- P2/restify/restify/property/models/property.py
+++ P2/restify/restify/property/models/property.py
@@ -3,12 +3,20 @@ from accounts.models.user_model import ThisUser
 
 # Create your models here.
 
+# amenities
+class Amenity(models.Model):
+    # these fields are requried
+    name = models.CharField(max_length=100)
+
 # property
 class Property(models.Model):
     # these fields are requried
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(ThisUser, on_delete=models.CASCADE)
+    province = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+    num_of_guests = models.IntegerField(blank=False)
+    amenities = models.ManyToManyField(Amenity, blank=True)
     # these fields are not required
     picture = models.ImageField(upload_to='property_pics', blank=True)
 
