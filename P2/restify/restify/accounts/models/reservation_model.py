43384bb43c3085fac53eb88b63a97702f54c50e0
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2023-03-14 12:32:05 -0400

    modify reservation

--- P2/restify/restify/accounts/models/reservation_model.py
+++ P2/restify/restify/accounts/models/reservation_model.py
@@ -1,12 +1,13 @@
 from django.db import models
-from property import models as PropertyModel
+from property.models import Property
 from .user_model import ThisUser
+
 
 # reservation
 class Reserve(models.Model):
-    user = models.ForeignKey(ThisUser, on_delete=models.CASCADE)
-    property = models.ForeignKey(PropertyModel.Property, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20)
-    request_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(ThisUser, on_delete=models.CASCADE, blank=True)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, blank=True)
+    status = models.CharField(max_length=20, blank=True)
+    request_date = models.DateTimeField(null=True)
     start_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
--- P2/restify/restify/accounts/urls.py
+++ P2/restify/restify/accounts/urls.py
@@ -10,21 +10,33 @@ urlpatterns = [
     path('create/', views.UserCreate.as_view(), name='create'),
 
     path('edit/<int:pk>/', views.UserEdit.as_view(), name='edit'),
+    #===================================================================================================
+    path('reservation/create/<int:property_id>/', views.ReservationCreate.as_view(), name='ReservationCreate'),
+    #===================================================================================================
 
     path('reservation/list/', views.ListReservation.as_view(), name='ListReservation'),
 
     path('reservation/list/<str:type_user>/', views.ListReservationFilterView.as_view(), name='ListReservationFilterView'),
 
+    #change status url
+
+    #host approve order
+    path('reservation/approve/<int:order_num>/', views.ApproveReservation.as_view(), name='ApproveReservation'),
+    #host/client cancel order
+    path('reservation/host-cancel/<int:order_num>/', views.HostCancelReservation.as_view(), name='CancelReservation'),
+    path('reservation/client-cancel/<int:order_num>/', views.ClientCancelReservation.as_view(), name='CancelReservation'),
+    #host terminate order
+    path('reservation/terminate/<int:order_num>/', views.TerminateReservation.as_view(), name='TerminateReservation'),
+    #client denied order
+    path('reservation/denied/<int:order_num>/', views.DeniedReservation.as_view(), name='DeniedReservation'),
 
 
     path('reservation/list/<str:type_user>/<str:state>/', views.ListReservationStateFilterView.as_view(), name='ListReservationStateFilterView'),
 
 
-
-
     path('reservation/list/<str:type_user>/<str:state>/<int:pk>/', views.ListReservationStateFilterView.as_view(), name='ListReservationStateFilterView'),
 
     
-    path('reservation/list/<str:type_user>/<str:state>/<str:action>/', views.DetailReservationStateUpdateView.as_view(), name='DetailReservationStateUpdateView'),
+    # path('reservation/list/<str:type_user>/<str:state>/<str:action>/', views.DetailReservationStateUpdateView.as_view(), name='DetailReservationStateUpdateView'),
 
 ]
